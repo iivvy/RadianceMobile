84832ab543e96d723172fddbaca9708dcddb58c5
Nixi <frraroua@gmail.com>
2023-08-24 20:14:06 +0100

    login page alomost done splsh screen not done

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -18,6 +18,9 @@ class _LoginState extends State<Login> {
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.only(top: 100, left: 20.0, right: 20.0),
+
+              // height: 521,
+
               child: Column(
                 children: <Widget>[
                   const Padding(
@@ -32,25 +35,44 @@ class _LoginState extends State<Login> {
                       ),
                     ),
                   ),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20.0),
-                      child: Text(
-                        'Email address',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 15,
-                            color: Colors.black),
-                      ),
-                    ),
+                  const SizedBox(
+                    height: 30,
                   ),
+                  // const Align(
+                  //   alignment: Alignment.centerLeft,
+                  //   child: Padding(
+                  //     padding: EdgeInsets.only(left: 20.0),
+                  //     child: Text(
+                  //       'Email address',
+                  //       style: TextStyle(
+                  //           fontFamily: 'Poppins',
+                  //           fontStyle: FontStyle.normal,
+                  //           fontWeight: FontWeight.w600,
+                  //           fontSize: 15,
+                  //           color: Colors.black),
+                  //     ),
+                  //   ),
+                  // ),
+
                   SizedBox(
-                    height: 350,
+                    height: 450,
                     child: Column(
                       children: [
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: Text(
+                              'Email address',
+                              style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontStyle: FontStyle.normal,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15,
+                                  color: Colors.black),
+                            ),
+                          ),
+                        ),
                         const Padding(
                           padding: EdgeInsets.only(
                               left: 20.0, top: 10.0, right: 20.0, bottom: 20.0),
@@ -96,7 +118,6 @@ class _LoginState extends State<Login> {
                             ),
                           ),
                         ),
-
                         const Padding(
                           padding: EdgeInsets.only(
                               left: 20.0, top: 10.0, right: 20.0, bottom: 20.0),
@@ -126,13 +147,15 @@ class _LoginState extends State<Login> {
                             ),
                           ),
                         ),
-                        //0f172A
+                        const SizedBox(
+                          height: 20.0,
+                        ),
                         Padding(
-                          padding: const EdgeInsets.only(top: 16.0),
+                          padding:const  EdgeInsets.only(top: 1.0, bottom: 5.0),
                           child: Material(
                             color: const Color(0xff0f172a),
                             borderRadius:
-                                const BorderRadius.all(Radius.circular(5.0)),
+                                const BorderRadius.all(Radius.circular(13.0)),
                             elevation: 2.0,
                             child: MaterialButton(
                                 minWidth: 240.0,
@@ -164,34 +187,49 @@ class _LoginState extends State<Login> {
                                 )),
                           ),
                         ),
-                        RichText(
-                          textAlign:TextAlign.center,
-                          text: const TextSpan(
-                            text: "By signing in, you confirm our",
-                            style: TextStyle(
-                                color: Color(0x898c8ca1),
-                                fontSize: 15,
-                                fontFamily: 'poppins'),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: 'Terms Of Use ',
-                                  style: TextStyle(
-                                      color: Color(0xff1d4ed8),
-                                      fontWeight: FontWeight.w700)),
-                              TextSpan(
-                                  text: 'and ',
-                                  style: TextStyle(color: Color(0x898c8ca1))),
-                              TextSpan(
-                                  text: 'Privacy Policy ',
-                                  style: TextStyle(
-                                      color: Color(0xff1d4ed8),
-                                      fontWeight: FontWeight.w700)),
-                            ],
-                          ),
-                        )
                       ],
                     ),
                   ),
+
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        text: "By signing in, you confirm our",
+                        style: TextStyle(
+                            color: Color(0x898c8ca1),
+                            fontSize: 15,
+                            fontFamily: 'poppins'),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Terms Of Use ',
+                              style: TextStyle(
+                                  color: Color(0xff1d4ed8),
+                                  fontWeight: FontWeight.w700)),
+                          TextSpan(
+                              text: 'and ',
+                              style: TextStyle(color: Color(0x898c8ca1))),
+                          TextSpan(
+                              text: 'Privacy Policy ',
+                              style: TextStyle(
+                                  color: Color(0xff1d4ed8),
+                                  fontWeight: FontWeight.w700)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  //  Expanded(
+                  //
+                  //   child: Align(
+                  //
+                  //     alignment: FractionalOffset.center,
+                  //     child:
+                  //   ),
+                  // ),
                 ],
               ),
             ),
