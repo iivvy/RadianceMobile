2d85bd4b916495e227fc52399f5ccdaf03a8e277
Nixi <frraroua@gmail.com>
2023-09-08 21:48:42 +0100

    modified the response type

--- lib/prediction/predict_repo.dart
+++ lib/prediction/predict_repo.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 
+
 import 'package:RadianceAI/Patients/models/patient_list_model.dart';
 import 'package:RadianceAI/main_repo.dart';
 import 'package:RadianceAI/prediction/models/predict_list_model.dart';
+
 import 'package:RadianceAI/radiance_config.dart';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
@@ -17,6 +20,89 @@ class PredictService extends MainRepository {
     String? token = await storage.read(key: 'access_token');
     return token;
   }
+
+
+Future<Map<String, dynamic>> predictImage(Patient patient, XFile frt) async {
+  try {
+    String? token = await getAccessToken();
+
+   print('access token $token');
+    final request = http.MultipartRequest('POST', Uri.parse(RadianceAI.predict));
+
+    request.headers.addAll({
+      'Accept': 'application/json',
+      'Content-Type': 'multipart/form-data',
+      'Authorization': 'Bearer $token',
+    });
+
+    request.fields.addAll({
+      'first_name': patient.first_name,
+      'last_name': patient.last_name,
+      'age': patient.age.toString(), // Convert age to a string
+      'history':patient.history!,
+    });
+
+    if (frt != null) {
+      request.files.add(await http.MultipartFile.fromPath(
+        'frontal_image',
+        frt.path,
+        contentType: MediaType('image', 'png'), // Adjust the content type as needed
+      ));
+    }
+    print(request.headers);
+
+    final response = await request.send();
+    // print("this is thw pred response ${jsonDecode(await response.stream.bytesToString())}");
+
+    if (response.statusCode == 200) {
+       final responseBody = await response.stream.bytesToString();
+      print("this is the response body ---------------------$responseBody");
+
+      final Map<String, dynamic> responseJson = jsonDecode(responseBody);
+      print("this is the responseJson body ---------------------$responseJson");
+
+      return responseJson;
+  
+      // return jsonDecode(responseBody); 
+    } else {
+      throw Exception('Failed to make the prediction request');
+    }
+  } catch (e) {
+    print('Error: $e');
+    throw e;
+  }
+}
+
+
+  Future<Uint8List?> fetchActivationMap(String predictionId) async {
+    final url = Uri.parse(RadianceAI.getPatientPrediction(predictionId));
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        return response.bodyBytes;
+      } else {
+        // Handle error cases here
+        print('Failed to fetch activation map: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      // Handle network errors here
+      print('Error fetching activation map: $e');
+      return null;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
 
   // Future<PredictionListModel> predictImage(Patient patient, XFile? frt) async {
   //   var accessToken = getAccessToken();
@@ -74,68 +160,3 @@ class PredictService extends MainRepository {
   //   //     throw Exception('Failed to load patients');
   //   //   }
   // }
-Future<Map<String, dynamic>> predictImage(Patient patient, XFile frt) async {
-  try {
-    String? token = await getAccessToken();
-
-   print('access token $token');
-    final request = http.MultipartRequest('POST', Uri.parse(RadianceAI.predict));
-
-    request.headers.addAll({
-      'Accept': 'application/json',
-      'Content-Type': 'multipart/form-data',
-      'Authorization': 'Bearer $token',
-    });
-
-    request.fields.addAll({
-      'first_name': patient.first_name,
-      'last_name': patient.last_name,
-      'age': patient.age.toString(), // Convert age to a string
-      'history':patient.history!,
-    });
-
-    if (frt != null) {
-      request.files.add(await http.MultipartFile.fromPath(
-        'frontal_image',
-        frt.path,
-        contentType: MediaType('image', 'png'), // Adjust the content type as needed
-      ));
-    }
-    print(request.headers);
-
-    final response = await request.send();
-    print("this is thw pred response ${jsonDecode(await response.stream.bytesToString())}");
-
-    if (response.statusCode == 200) {
-      final responseBody = await response.stream.bytesToString();
-      return jsonDecode(responseBody);
-    } else {
-      throw Exception('Failed to make the prediction request');
-    }
-  } catch (e) {
-    print('Error: $e');
-    throw e;
-  }
-}
-
-
-  Future<Uint8List?> fetchActivationMap(String predictionId) async {
-    final url = Uri.parse(RadianceAI.getPatientPrediction(predictionId));
-
-    try {
-      final response = await http.get(url);
-
-      if (response.statusCode == 200) {
-        return response.bodyBytes;
-      } else {
-        // Handle error cases here
-        print('Failed to fetch activation map: ${response.statusCode}');
-        return null;
-      }
-    } catch (e) {
-      // Handle network errors here
-      print('Error fetching activation map: $e');
-      return null;
-    }
-  }
-}
